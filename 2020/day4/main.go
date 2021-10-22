b00d28f63948d440dbe7a12a1f7970224dd7af8d
Fahmi Ardhianto <ardhianto@accelbyte.net>
2021-10-22 21:50:32 +0700

    day4 part2 solution

--- 2020/day4/main.go
+++ 2020/day4/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -20,25 +22,96 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(SplitBlankLine)
 
-	required := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
 	validCount := 0
 	for scanner.Scan() {
-		passport := scanner.Text()
-		valid := true
-		for _, r := range required {
-			containsRequired := strings.Contains(passport, r)
-			if !containsRequired {
-				valid = false
-				break
-			}
-
-		}
+		replacedNewLine := strings.ReplaceAll(scanner.Text(), "\n", " ")
+		passportData := strings.TrimRight(replacedNewLine, " ")
+		valid := isValidPassport(strings.Split(passportData, " "))
 		if valid {
 			validCount++
 		}
 	}
 
 	fmt.Println(validCount)
+}
+
+func isValidPassport(data []string) bool {
+	checkBit := 0
+	for _, pair := range data {
+		kv := strings.Split(pair, ":")
+		key := kv[0]
+		val := kv[1]
+
+		switch key {
+		case "byr":
+			year, err := strconv.Atoi(val)
+			if err != nil {
+				continue
+			}
+			if year >= 1920 && year <= 2002 {
+				checkBit = checkBit | 1
+			}
+		case "iyr":
+			year, err := strconv.Atoi(val)
+			if err != nil {
+				continue
+			}
+			if year >= 2010 && year <= 2020 {
+				checkBit = checkBit | 1<<1
+			}
+		case "eyr":
+			year, err := strconv.Atoi(val)
+			if err != nil {
+				continue
+			}
+			if year >= 2020 && year <= 2030 {
+				checkBit = checkBit | 1<<2
+			}
+		case "hgt":
+			if strings.HasSuffix(val, "cm") {
+				h, err := strconv.Atoi(strings.TrimSuffix(val, "cm"))
+				if err != nil {
+					continue
+				}
+				if h >= 150 && h <= 193 {
+					checkBit = checkBit | 1<<3
+				}
+			}
+			if strings.HasSuffix(val, "in") {
+				h, err := strconv.Atoi(strings.TrimSuffix(val, "in"))
+				if err != nil {
+					continue
+				}
+				if h >= 59 && h <= 76 {
+					checkBit = checkBit | 1<<3
+				}
+			}
+		case "hcl":
+			_, err := hex.DecodeString(strings.TrimPrefix(val, "#"))
+			if err != nil || !strings.HasPrefix(val, "#") {
+				continue
+			}
+			checkBit = checkBit | 1<<4
+		case "ecl":
+			for _, v := range []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"} {
+				if val == v {
+					checkBit = checkBit | 1<<5
+					break
+				}
+			}
+		case "pid":
+			_, err := strconv.Atoi(val)
+			if err == nil && len(val) == 9 {
+				checkBit = checkBit | 1<<6
+			}
+		}
+	}
+
+	if checkBit == 1<<7-1 {
+		return true
+	}
+
+	return false
 }
 
 func SplitBlankLine(data []byte, atEOF bool) (advance int, token []byte, err error) {
